f8d3bda0c0636318a349c2504b1e9c111d249f68
rhrh9999 <osht9499@naver.com>
2022-10-30 16:45:14 +0900

    [update] dialog

---
+++ reactfolder/src/dialog/Dialog.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './Dialog.css';
+
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-'}>
+      {props.children}
+    </div>
+  );
+}
+
+function Dialog(props) {
+  return (
+    <FancyBorder>
+      <h1 className="Dialog-title">
+        {props.title}
+      </h1>
+      <p className="Dialog-message">
+        {props.message}
+      </p>
+    </FancyBorder>
+  );
+}
+
+export default Dialog;
